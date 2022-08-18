28869291b3ec2cf18507b66028406245be07b7dd
Abhijeet Chaudhari <abhijeetchaudhari615@gmail.com>
2022-08-18 14:15:51 +0530

    frontEnd init commit

---
+++ chatapp/client/src/Allroutes/Allroutes.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { ChatPage } from "../pages/Chatpage";
+import { HomePage } from "../pages/Homepage";
+
+export const Allroutes = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/chats" element={<ChatPage />}></Route>
+      </Routes>
+    </div>
+  );
+};
